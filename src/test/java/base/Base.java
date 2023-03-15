3ea8eb15c521aec745b6f631f80a2c8e0f7d73f2
Abubakar-88 <76661821+Abubakar-88@users.noreply.github.com>
2023-03-15 12:54:33 +0600

    Update Base.java

--- src/test/java/base/Base.java
+++ src/test/java/base/Base.java
@@ -12,28 +12,28 @@ public class Base {
 	
 	public static void click(By by) {
 
-		browser.findElement(by).click();
+		driver.findElement(by).click();
 	}
 	public static void sendkeys(By by,String name) {
-		browser.findElement(by).sendKeys(name);
+		driver.findElement(by).sendKeys(name);
 	}
 	public static boolean isDisplayed(By by) {
-		return browser.findElement(by).isDisplayed();
+		return driver.findElement(by).isDisplayed();
 	}
 	public static boolean isEnabled(By by) {
-		return browser.findElement(by).isEnabled();
+		return driver.findElement(by).isEnabled();
 	}
 	public static String getText(By by) {
-		return browser.findElement(by).getText();
+		return driver.findElement(by).getText();
 	}
 	public static void navigateURL(String url) {
-		browser.get(url);
+		driver.get(url);
 	}
 	public static void clear(By by) {
-		browser.findElement(by).clear();
+		driver.findElement(by).clear();
 	}
 	public  String getAttributeValue(By by, String key ){
-		return browser.findElement( by ).getAttribute( key );
+		return driver.findElement( by ).getAttribute( key );
 	}
 
 	
