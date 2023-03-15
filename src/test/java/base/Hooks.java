f7ce6115783d417e2fb7a6425901434d7742aac5
Abubakar-88 <76661821+Abubakar-88@users.noreply.github.com>
2023-03-15 12:51:18 +0600

    Update Hooks.java

--- src/test/java/base/Hooks.java
+++ src/test/java/base/Hooks.java
@@ -1,11 +1,14 @@
 package base;
 
-import java.util.concurrent.TimeUnit;
-
+mport org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.ScreenshotException;
 
+import base.Base;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -14,13 +17,25 @@ public class Hooks extends Base{
 	
 	@Before
 	public void setup() {
-		ChromeOptions option = new ChromeOptions();
-		WebDriverManager.chromedriver().setup();
-		browser = new ChromeDriver(option);
-		browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		//ChromeOptions option = new ChromeOptions();
+		//WebDriverManager.chromedriver().setup();
+		//browser = new ChromeDriver(option);
+		//browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		//JENKIN
-		
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		driver = new RemoteWebDriver(capabilities);
+		Capabilities actualCapabilities = ((RemoteWebDriver) driver).getCapabilities();
 	}
+	
+		public String extractScreenShot(WebDriverException e) {
+		  Throwable cause = e.getCause();
+		  if (cause instanceof ScreenshotException) {
+		    return ((ScreenshotException) cause).getBase64EncodedScreenshot();
+		  }
+               return null;
+	}	
+	
+	
 	@After
 	public void tearDown() {
 		//browser.close();
